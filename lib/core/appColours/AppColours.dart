621fd0298459277d6d80deb00e9d06bbeb99f267
EyadWaleid <eyadwaleedabdelfatah@gmail.com>
2026-06-24 01:58:28 +0300

    added mostly all the app assets

---
+++ lib/core/appColours/AppColours.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColours {
+  static Color primaryColour = const Color(0xFF5669FF);
+  static Color blackColour = const Color(0xFF120D26);
+  static Color greyColour = const Color(0xFF747688);
+  static Color secondeColour = const Color(0xFF3D55F0);
+  static Color backgroundColour = const Color(0xFFDADADA);
+  static Color darkGreyColour = const Color(0xFF9D9898);
+}
---
+++ lib/core/appTextStyles/AppTextStyles.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+abstract class AppTextStyles {
+  static final TextStyle fontStyle22 = TextStyle(
+    fontSize: 22,
+    fontFamily: "Manrope-Medium",
+  );
+  static final TextStyle fontStyle15 = TextStyle(
+    fontSize: 15,
+    fontFamily: "Manrope-Medium",
+  );
+  static final TextStyle fontStyle18 = TextStyle(
+    fontSize: 18,
+    fontFamily: "Manrope-Medium",
+  );
+  static final TextStyle fontStyle16 = TextStyle(
+    fontSize: 16,
+    fontFamily: "Manrope-Medium",
+  );
+}
